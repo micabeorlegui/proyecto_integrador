eee3ce931d5bba354746cfb208521db62cadcd1f
jazbang <jbang@udesa.edu.ar>
2023-11-11 19:41:38 -0300

    detail-movie.js (parte de ver recomendaciones), agregue estructura html para esta parte de la consigna e hice su css. Falta que al clickear en una de las recomendaciones, lleve al dealle de esa pelicula espcifica

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -6,48 +6,97 @@ fetch(`https://api.themoviedb.org/3/movie/${idPelicula}?api_key=${apiKey}&langua
         return response.json();
     })
     .then(function(data){
-        console.log(data)
         let fotoDePortada= data.poster_path;
         let titulo= data.title;
         let calificacion= data.vote_average;
         let estreno= data.release_date;
         let duracion= data.runtime;
         let sinopsis= data.overview;
-        let genero_nombres= []
-        let generes_info_api= data.genres
-        for (let i=0; i<generes_info_api.length; i++){
-            let nombre= generes_info_api[i].name;
-            genero_nombres.push(nombre);
+        let generoNombres= []
+        let generesInfoApi= data.genres
+        for (let i=0; i<generesInfoApi.length; i++){
+            let nombre= generesInfoApi[i].name;
+            generoNombres.push(nombre);
         };
 
-        let section_detail= document.querySelector('section');
-        let imagen= section_detail.querySelector('img');
-        let h2_titulo= section_detail.querySelector('div h2');
-        let lista_li= section_detail.querySelectorAll('div ul li');
+        let sectionDetail= document.querySelector('section');
+        let imagen= sectionDetail.querySelector('img');
+        let h2Titulo= sectionDetail.querySelector('div h2');
+        let listaLi= sectionDetail.querySelectorAll('div ul li');
 
         let baseUrl= "https://image.tmdb.org/t/p/w500";
         imagen.src= `${baseUrl}` + `${fotoDePortada}`;
         imagen.alt=`${titulo}`;
-        h2_titulo.innerText=`${titulo}`;
-        for (let i=0; i<lista_li.length; i++){
+        h2Titulo.innerText=`${titulo}`;
+        for (let i=0; i<listaLi.length; i++){
             if(i==0){
-                lista_li[i].innerHTML=`<b>Calificación:</b> ${calificacion}`;
+                listaLi[i].innerHTML=`<b>Calificación:</b> ${calificacion}`;
             } else if (i==1){
-                lista_li[i].innerHTML=`<b>Fecha de estreno:</b> ${estreno}`;
+                listaLi[i].innerHTML=`<b>Fecha de estreno:</b> ${estreno}`;
             } else if (i==2){
-                lista_li[i].innerHTML=`<b>Duración:</b> ${duracion} m`;
+                listaLi[i].innerHTML=`<b>Duración:</b> ${duracion} m`;
             } else if (i==3){
-                lista_li[i].innerHTML=`<b>Sinopsis:</b> ${sinopsis}`;
+                listaLi[i].innerHTML=`<b>Sinopsis:</b> ${sinopsis}`;
             } else{
-                if (genero_nombres.length>1){
-                    lista_li[i].innerHTML=`<a href="genres.html"><b>Géneros:</b> ${genero_nombres.join(', ')}</a>`; 
+                if (generoNombres.length>1){
+                    listaLi[i].innerHTML=`<a href="genres.html"><b>Géneros:</b> ${generoNombres.join(', ')}</a>`; 
                 } else{
-                    lista_li[i].innerHTML=`<a href="genres.html"><b>Género:</b> ${genero_nombres}</a>`;
+                    listaLi[i].innerHTML=`<a href="genres.html"><b>Género:</b> ${generoNombres}</a>`;
                 };
             };
         };
+    })
+    .catch(function(error){
+        console.log('El error es: ' + error)
+    })
 
+fetch(`https://api.themoviedb.org/3/movie/${idPelicula}/recommendations?api_key=${apiKey}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let pelisRecomendaciones= data.results;
+        let imgRecomedaciones=[];
+        let titulosRecomendaciones=[];
+        for (let i=0; i<pelisRecomendaciones.length; i++){
+            let imagen= pelisRecomendaciones[i].poster_path;
+            imgRecomedaciones.push(imagen);
+            let titulo= pelisRecomendaciones[i].title;
+            titulosRecomendaciones.push(titulo);
+        };
+        
+        let cincoNumRandom= [];
+        for(let i=0; i<5; i++){
+            let numRandom= Math.floor(Math.random() * imgRecomedaciones.length);
+            if (!cincoNumRandom.includes(numRandom)){
+                cincoNumRandom.push(numRandom)
+            }else{
+                i--;
+            };
+        };
 
+        let imgCincoRecomendaciones= []
+        let tituloCincoRecomendaciones=[]
+        for (let i=0; i<5; i++){
+            imgCincoRecomendaciones.push(imgRecomedaciones[cincoNumRandom[i]]);
+            tituloCincoRecomendaciones.push(titulosRecomendaciones[cincoNumRandom[i]])
+        };
+
+        let boton= document.querySelector('#recomendaciones_boton');
+        boton.addEventListener('click', function(){
+            let recomendacionesSect= document.querySelector('.recomendaciones');
+            let divs= recomendacionesSect.querySelectorAll('div');
+            let imagenes= recomendacionesSect.querySelectorAll('div a img');
+            let baseUrl= "https://image.tmdb.org/t/p/w500";
+            for (let i=0; i<imagenes.length; i++){
+                imagenes[i].src = `${baseUrl}${imgCincoRecomendaciones[i]}`;
+                imagenes[i].alt = `${tituloCincoRecomendaciones[i]}`;
+            };
+            for (let i=0; i<divs.length; i++){
+                divs[i].style.display= 'inline-block';
+            };
+            
+        });
     })
     .catch(function(error){
         console.log('El error es: ' + error)
