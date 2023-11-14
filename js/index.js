818285a403edc89a87bde53d28a27f17d5754ea9
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2023-11-14 09:56:40 -0300

    agregue info a los html

--- js/index.js
+++ js/index.js
@@ -195,3 +195,11 @@ fetch(urlSeries)
         .catch(function(error){
             console.log('El error es: ' + error);
         })       
+
+let busqueda= document.querySelector('#searchForm');
+busqueda.addEventListener("submit", function(event){
+    event.preventDefault();
+    let inputTexto = document.querySelector('#searchInput').value;
+    document.querySelector('#searchResults').innerHTML= ` Resultado de búsqueda para: ${inputTexto}`;
+})
+
