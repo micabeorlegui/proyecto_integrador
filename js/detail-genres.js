3b75f2da4d34f453f0db0eeda560298895676129
jazbang <jbang@udesa.edu.ar>
2023-11-16 20:07:19 -0300

    corrección de endpoints detail-genres

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,8 +1,8 @@
 let apiKey = 'd4da6f83d8fa5dad990cafe88cb4fbf7'
 let urlparams = new URLSearchParams(location.search);
 let qsIdGenero = urlparams.get('id');
-let DiscoverEndpointPelis = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es&page=1&sort_by=popularity.desc`;
-let DiscoverEndpointSeries = `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=es&page=1&sort_by=popularity.desc`;
+let DiscoverEndpointPelis = `https://api.themoviedb.org/3/discover/movie`;
+let DiscoverEndpointSeries = `https://api.themoviedb.org/3/discover/tv`;
 let detailGenresPelis = `${DiscoverEndpointPelis}?api_key=${apiKey}&with_genres=${qsIdGenero}`;
 let detailGenresSeries = `${DiscoverEndpointSeries}?api_key=${apiKey}&with_genres=${qsIdGenero}`;
 
