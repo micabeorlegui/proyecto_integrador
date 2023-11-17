959ef595617a063795521328f1e78f8c46a5047c
jazbang <jbang@udesa.edu.ar>
2023-11-16 21:38:26 -0300

    index.js- agregue código para poder agregar el id de las peliculas/series/recomendaciones que figuran en el home en el href de la etiqueta a de cada una de ellas para poder hacer que en detalle aparezca la info correspondiente ded cada una

--- js/index.js
+++ js/index.js
@@ -12,9 +12,7 @@ fetch(urlPelicula)
         let titulosPeliculas= [];
         let fotosDePortadasPelis= [];
         let estrenosPelis= [];
-       /// console.log(titulosPeliculas)
-       /// console.log(fotosDePortadasPelis)
-       ///console.log(estrenosPelis)
+        let idsPelis= [];
 
         for (let i=0; i<peliculasPopulares.length; i++){
             let titulo= peliculasPopulares[i].title;
@@ -23,7 +21,9 @@ fetch(urlPelicula)
             let imagen= peliculasPopulares[i].poster_path;
             fotosDePortadasPelis.push(imagen);
             let estreno= peliculasPopulares[i].release_date;
-            estrenosPelis.push(estreno)
+            estrenosPelis.push(estreno);
+            let id= peliculasPopulares[i].id;
+            idsPelis.push(id);
             }
         };
 
@@ -31,24 +31,24 @@ fetch(urlPelicula)
         for(let i=0; i<5; i++){
             let numRandom= Math.floor(Math.random() * titulosPeliculas.length);
             if (!cincoNumRandom.includes(numRandom)){
-                cincoNumRandom.push(numRandom)
+                cincoNumRandom.push(numRandom);
             }else{
                 i--;
             };
         };
 
-        let tituloCincoPelis=[]
-        let imgCincoPelis= []
-        let estrenoCincoPelis=[]
-        console.log(tituloCincoPelis)
+        let tituloCincoPelis=[];
+        let imgCincoPelis= [];
+        let estrenoCincoPelis=[];
+        let idCincoPelis=[];
+        console.log(idCincoPelis)
         for (let i=0; i<5; i++){
             tituloCincoPelis.push(titulosPeliculas[cincoNumRandom[i]]);
             imgCincoPelis.push(fotosDePortadasPelis[cincoNumRandom[i]]);
             estrenoCincoPelis.push(estrenosPelis[cincoNumRandom[i]])
+            idCincoPelis.push(idsPelis[cincoNumRandom[i]]);
         };
 
-
-        //invoco
         let sectionPeliculas= document.querySelector('.sect_uno');
         let divsPeliculas= sectionPeliculas.querySelectorAll('div');
 
@@ -58,12 +58,14 @@ fetch(urlPelicula)
             let h3Titulo= divsPeliculas[i].querySelector('h3');
             let imagen= divsPeliculas[i].querySelector('img');
             let parrafo= divsPeliculas[i].querySelector('p');
+            let a= divsPeliculas[i].querySelector('a')
             
             h3Titulo.innerText=`${tituloCincoPelis[i]}`;
             let baseUrl= "https://image.tmdb.org/t/p/w500"; 
             imagen.src= `${baseUrl}` + `${imgCincoPelis[i]}`; 
             imagen.alt=`${tituloCincoPelis[i]}`;  
             parrafo.innerText= `Estreno: ${estrenoCincoPelis[i]}`;
+            a.href+=`?id=${idCincoPelis[i]}`
 
         };
 
@@ -84,6 +86,7 @@ fetch(urlSeries)
         let titulosSeries=[];
         let fotosDePortadasSeries=[];
         let estrenosSeries=[];
+        let idsSeries= [];
 
         for (let i=0; i<seriesPopulares.length; i++){
             let titulo= seriesPopulares[i].name;
@@ -93,6 +96,8 @@ fetch(urlSeries)
             fotosDePortadasSeries.push(imagen);
             let estreno= seriesPopulares[i].first_air_date;
             estrenosSeries.push(estreno)
+            let id= seriesPopulares[i].id;
+            idsSeries.push(id);
             }
         };
        
@@ -109,11 +114,12 @@ fetch(urlSeries)
         let tituloCincoSeries=[]
         let imgCincoSeries= []
         let estrenoCincoSeries=[]
-        console.log(tituloCincoSeries)
+        let idCincoSeries=[];
         for (let i=0; i<5; i++){
             tituloCincoSeries.push(titulosSeries[cincoNumRandom[i]]);
             imgCincoSeries.push(fotosDePortadasSeries[cincoNumRandom[i]]);
-            estrenoCincoSeries.push(estrenosSeries[cincoNumRandom[i]])
+            estrenoCincoSeries.push(estrenosSeries[cincoNumRandom[i]]);
+            idCincoSeries.push(idsSeries[cincoNumRandom[i]]);
         };
 
         let sectionSeries= document.querySelector('.sect_dos');
@@ -125,12 +131,14 @@ fetch(urlSeries)
             let h3Titulo= divsSeries[i].querySelector('h3');
             let imagen= divsSeries[i].querySelector('img');
             let parrafo= divsSeries[i].querySelector('p');
+            let a= divsSeries[i].querySelector('a')
             
             h3Titulo.innerText=`${tituloCincoSeries[i]}`;
             let baseUrl= "https://image.tmdb.org/t/p/w500"; 
             imagen.src= `${baseUrl}` + `${imgCincoSeries[i]}`; 
             imagen.alt=`${tituloCincoSeries[i]}`;  
             parrafo.innerText= `Estreno: ${estrenoCincoSeries[i]}`;
+            a.href+=`?id=${idCincoSeries[i]}`
 
         };
     })
@@ -150,6 +158,7 @@ fetch(urlSeries)
             let titulosRecomendados=[];
             let fotosDePortadasRecomendados=[];
             let estrenosRecomendados=[];
+            let idsRecomendados= [];
 
             for (let i=0; i<recomendados.length; i++){
                 let titulo= recomendados[i].title;
@@ -158,7 +167,9 @@ fetch(urlSeries)
                 let imagen= recomendados[i].poster_path;
                 fotosDePortadasRecomendados.push(imagen);
                 let estreno= recomendados[i].release_date;
-                estrenosRecomendados.push(estreno)
+                estrenosRecomendados.push(estreno);
+                let id= recomendados[i].id;
+                idsRecomendados.push(id)
                 }
             };
 
@@ -166,20 +177,22 @@ fetch(urlSeries)
             for(let i=0; i<5; i++){
                 let numRandom= Math.floor(Math.random() * titulosRecomendados.length);
                 if (!cincoNumRandom.includes(numRandom)){
-                    cincoNumRandom.push(numRandom)
+                    cincoNumRandom.push(numRandom);
                 }else{
                     i--;
                 };
             }
         
-            let tituloCincoRecomendados=[]
-            let imgCincoRecomendados= []
-            let estrenoCincoRecomendados=[]
+            let tituloCincoRecomendados=[];
+            let imgCincoRecomendados= [];
+            let estrenoCincoRecomendados=[];
+            let idCincoRecomendados=[];
             console.log(tituloCincoRecomendados)
             for (let i=0; i<5; i++){
                 tituloCincoRecomendados.push(titulosRecomendados[cincoNumRandom[i]]);
                 imgCincoRecomendados.push(fotosDePortadasRecomendados[cincoNumRandom[i]]);
-                estrenoCincoRecomendados.push(estrenosRecomendados[cincoNumRandom[i]])
+                estrenoCincoRecomendados.push(estrenosRecomendados[cincoNumRandom[i]]);
+                idCincoRecomendados.push(idsRecomendados[cincoNumRandom[i]])
             };
 
             let sectionRecomendados= document.querySelector('.sect_tres');
@@ -189,12 +202,14 @@ fetch(urlSeries)
                 let h3Titulo= divsRecomendados[i].querySelector('h3');
                 let imagen= divsRecomendados[i].querySelector('img');
                 let parrafo= divsRecomendados[i].querySelector('p');
+                let a= divsRecomendados[i].querySelector('a');
                 
                 h3Titulo.innerText=`${tituloCincoRecomendados[i]}`;
                 let baseUrl= "https://image.tmdb.org/t/p/w500"; 
                 imagen.src= `${baseUrl}` + `${imgCincoRecomendados[i]}`; 
                 imagen.alt=`${tituloCincoRecomendados[i]}`;  
                 parrafo.innerText= `Estreno: ${estrenoCincoRecomendados[i]}`;
+                a.href+=`?id=${idCincoRecomendados[i]}`
     
             }; 
         })
