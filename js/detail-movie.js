8d8f8c6591ebe4073da4cffd2c1078845e620b41
jazbang <jbang@udesa.edu.ar>
2023-11-16 22:06:04 -0300

    detail-movie.js completo- use querystring id para que me de la info de esa pelicula. también termine la funcionalidad opcional de reseñas (al apretar me redirige al detalle de esa pelicula

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,187 +1,194 @@
-let apiKey= 'd4da6f83d8fa5dad990cafe88cb4fbf7';
-let idPelicula= 157350;
+document.addEventListener("DOMContentLoaded", function(){
+    let queryString=location.search;
+    let queryStringObj= new URLSearchParams(queryString);
+    let idPelicula= queryStringObj.get('id');
 
-fetch(`https://api.themoviedb.org/3/movie/${idPelicula}?api_key=${apiKey}&language=es`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        let fotoDePortada= data.poster_path;
-        let titulo= data.title;
-        let calificacion= data.vote_average;
-        let estreno= data.release_date;
-        let duracion= data.runtime;
-        let sinopsis= data.overview;
-        let generoNombres= []
-        let generesInfoApi= data.genres
-        for (let i=0; i<generesInfoApi.length; i++){
-            let nombre= generesInfoApi[i].name;
-            generoNombres.push(nombre);
-        };
+    let apiKey= 'd4da6f83d8fa5dad990cafe88cb4fbf7';
 
-        let sectionDetail= document.querySelector('section');
-        let imagen= sectionDetail.querySelector('img');
-        let h2Titulo= sectionDetail.querySelector('div h2');
-        let listaLi= sectionDetail.querySelectorAll('div ul li');
-
-        let baseUrl= "https://image.tmdb.org/t/p/w500";
-        imagen.src= `${baseUrl}` + `${fotoDePortada}`;
-        imagen.alt=`${titulo}`;
-        h2Titulo.innerText=`${titulo}`;
-        for (let i=0; i<listaLi.length; i++){
-            if(i==0){
-                listaLi[i].innerHTML=`<b>Calificación:</b> ${calificacion}`;
-            } else if (i==1){
-                listaLi[i].innerHTML=`<b>Fecha de estreno:</b> ${estreno}`;
-            } else if (i==2){
-                listaLi[i].innerHTML=`<b>Duración:</b> ${duracion} m`;
-            } else if (i==3){
-                listaLi[i].innerHTML=`<b>Sinopsis:</b> ${sinopsis}`;
-            } else{
-                if (generoNombres.length>1){
-                    listaLi[i].innerHTML=`<a href="genres.html"><b>Géneros:</b> ${generoNombres.join(', ')}</a>`; 
-                } else{
-                    listaLi[i].innerHTML=`<a href="genres.html"><b>Género:</b> ${generoNombres}</a>`;
-                };
-            };
-        };
-    })
-    .catch(function(error){
-        console.log('El error es: ' + error)
-    })
-
-fetch(`https://api.themoviedb.org/3/movie/${idPelicula}/recommendations?api_key=${apiKey}`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        let pelisRecomendaciones= data.results;
-        let imgRecomedaciones=[];
-        let titulosRecomendaciones=[];
-        let idsRecomendaciones=[];
-        for (let i=0; i<pelisRecomendaciones.length; i++){
-            let imagen= pelisRecomendaciones[i].poster_path;
-            imgRecomedaciones.push(imagen);
-            let titulo= pelisRecomendaciones[i].title;
-            titulosRecomendaciones.push(titulo);
-            let id= pelisRecomendaciones[i].id;
-            idsRecomendaciones.push(id)
-        };
-        
-        let cincoNumRandom= [];
-        for(let i=0; i<5; i++){
-            let numRandom= Math.floor(Math.random() * imgRecomedaciones.length);
-            if (!cincoNumRandom.includes(numRandom)){
-                cincoNumRandom.push(numRandom)
-            }else{
-                i--;
-            };
-        };
-
-        let imgCincoRecomendaciones= []
-        let tituloCincoRecomendaciones=[]
-        let idsCincoRecomendaciones=[]
-        for (let i=0; i<5; i++){
-            imgCincoRecomendaciones.push(imgRecomedaciones[cincoNumRandom[i]]);
-            tituloCincoRecomendaciones.push(titulosRecomendaciones[cincoNumRandom[i]]);
-            idsCincoRecomendaciones.push(idsRecomendaciones[cincoNumRandom[i]])
-        };
-
-        let boton= document.querySelector('#recomendaciones_boton');
-        let recomendacionesSect= document.querySelector('.recomendaciones');
-        let divs= recomendacionesSect.querySelectorAll('div');
-        let imagenes= recomendacionesSect.querySelectorAll('div a img');
-
-        boton.addEventListener('click', function(){
-            let baseUrl= "https://image.tmdb.org/t/p/w500";
-            for (let i=0; i<imagenes.length; i++){
-                imagenes[i].src = `${baseUrl}${imgCincoRecomendaciones[i]}`;
-                imagenes[i].alt = `${tituloCincoRecomendaciones[i]}`;
-            };
-            for (let i=0; i<divs.length; i++){
-                divs[i].style.display= 'inline-block';
-            };    
-        });
-
-
-    })
-    .catch(function(error){
-        console.log('El error es: ' + error)
-    })
-
-fetch(`https://api.themoviedb.org/3/movie/${idPelicula}/reviews?api_key=${apiKey}&language=es`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        let resenasPeliculas= data.results;
-
-        let botonRes= document.querySelector('#resenas_boton');
-        let resenasSect= document.querySelector('.resenas');
-        let listaLi= resenasSect.querySelector('div ul li');
-
-        if(resenasPeliculas.length>0){
-            for(let i=0; i<resenasPeliculas.length; i++){
-                let autor= resenasPeliculas[i].author;
-                let comentario= resenasPeliculas[i].content;
-
-                if(i==0){
-                    listaLi.innerHTML=`<b>${autor}</b>: ${comentario}`
-                }else{
-                    resenasSect.innerHTML+=`<div>
-                                                <ul>
-                                                    <li><b>${autor}</b>: ${comentario}</li>
-                                                </ul>
-                                            </div>`
-                };
-            };
-
-            let divsResenas= resenasSect.querySelectorAll('div');
-
-            for(let i=0; i<divsResenas.length; i++){
-                botonRes.addEventListener('click', function(){
-                    divsResenas[i].style.display= 'inline-block';
-                });
-            }
-
-        }else{
-            listaLi.innerHTML=`<strong>Lo siento, no hay reseñas disponibles :(</strong>`
-            let div= resenasSect.querySelector('div');
-            botonRes.addEventListener('click', function(){
-                div.style.display= 'inline-block';
-                div.style.backgroundColor= 'Bisque';
-                listaLi.style.color= 'rgb(247, 134, 134)';
-            });
-        };
-
-    })
-    .catch(function(error){
-        console.log('El error es: ' + error)
-    })
-
-    fetch(`https://api.themoviedb.org/3/movie/${idPelicula}/videos?api_key=${apiKey}&language=es`)
+    fetch(`https://api.themoviedb.org/3/movie/${idPelicula}?api_key=${apiKey}&language=es`)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
-            console.log(data)
-            let trailer= data.results[0];
-            let trailerId= trailer.id;
-            let youtube= trailer.site;
+            let fotoDePortada= data.poster_path;
+            let titulo= data.title;
+            let calificacion= data.vote_average;
+            let estreno= data.release_date;
+            let duracion= data.runtime;
+            let sinopsis= data.overview;
+            let generoNombres= [];
+            let generesInfoApi= data.genres;
+            for (let i=0; i<generesInfoApi.length; i++){
+                let nombre= generesInfoApi[i].name;
+                generoNombres.push(nombre);
+            };
 
-            let trailerSect= document.querySelector('.trailer');
+            let sectionDetail= document.querySelector('section');
+            let imagen= sectionDetail.querySelector('img');
+            let h2Titulo= sectionDetail.querySelector('div h2');
+            let listaLi= sectionDetail.querySelectorAll('div ul li');
 
-            if (youtube === 'YouTube') {
-                let trailerUrl = 'https://www.youtube.com/watch?v=' + trailerId;
-                let iframe= trailerSect.querySelector('iframe');
-                iframe.src=`${trailerUrl}`;
-                iframe.style.display='inline-block'
-            } else {
-                let parrafo= trailerSect.querySelector('p');
-                parrafo.innerHTML=`<strong>Lo siento, no hay trailer disponible para esta película :(</strong>`;
-                parrafo.style.display='inline-block';
+            let baseUrl= "https://image.tmdb.org/t/p/w500";
+            imagen.src= `${baseUrl}` + `${fotoDePortada}`;
+            imagen.alt=`${titulo}`;
+            h2Titulo.innerText=`${titulo}`;
+            for (let i=0; i<listaLi.length; i++){
+                if(i==0){
+                    listaLi[i].innerHTML=`<b>Calificación:</b> ${calificacion}`;
+                } else if (i==1){
+                    listaLi[i].innerHTML=`<b>Fecha de estreno:</b> ${estreno}`;
+                } else if (i==2){
+                    listaLi[i].innerHTML=`<b>Duración:</b> ${duracion} m`;
+                } else if (i==3){
+                    listaLi[i].innerHTML=`<b>Sinopsis:</b> ${sinopsis}`;
+                } else{
+                    if (generoNombres.length>1){
+                        listaLi[i].innerHTML=`<a href="genres.html"><b>Géneros:</b> ${generoNombres.join(', ')}</a>`; 
+                    } else{
+                        listaLi[i].innerHTML=`<a href="genres.html"><b>Género:</b> ${generoNombres}</a>`;
+                    };
+                };
             };
         })
         .catch(function(error){
             console.log('El error es: ' + error)
         })
+
+    fetch(`https://api.themoviedb.org/3/movie/${idPelicula}/recommendations?api_key=${apiKey}`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            let pelisRecomendaciones= data.results;
+            let imgRecomedaciones=[];
+            let titulosRecomendaciones=[];
+            let idsRecomendaciones=[];
+            for (let i=0; i<pelisRecomendaciones.length; i++){
+                let imagen= pelisRecomendaciones[i].poster_path;
+                imgRecomedaciones.push(imagen);
+                let titulo= pelisRecomendaciones[i].title;
+                titulosRecomendaciones.push(titulo);
+                let id= pelisRecomendaciones[i].id;
+                idsRecomendaciones.push(id)
+            };
+        
+            let cincoNumRandom= [];
+            for(let i=0; i<5; i++){
+                let numRandom= Math.floor(Math.random() * imgRecomedaciones.length);
+                if (!cincoNumRandom.includes(numRandom)){
+                    cincoNumRandom.push(numRandom)
+                }else{
+                    i--;
+                };
+            };
+
+            let imgCincoRecomendaciones= []
+            let tituloCincoRecomendaciones=[]
+            let idsCincoRecomendaciones=[]
+            for (let i=0; i<5; i++){
+                imgCincoRecomendaciones.push(imgRecomedaciones[cincoNumRandom[i]]);
+                tituloCincoRecomendaciones.push(titulosRecomendaciones[cincoNumRandom[i]]);
+                idsCincoRecomendaciones.push(idsRecomendaciones[cincoNumRandom[i]])
+            };
+
+            let boton= document.querySelector('#recomendaciones_boton');
+            let recomendacionesSect= document.querySelector('.recomendaciones');
+            let divs= recomendacionesSect.querySelectorAll('div');
+            let a= recomendacionesSect.querySelectorAll('a');
+            let imagenes= recomendacionesSect.querySelectorAll('img');
+
+            boton.addEventListener('click', function(){
+                let baseUrl= "https://image.tmdb.org/t/p/w500";
+                for (let i=0; i<imagenes.length; i++){
+                    imagenes[i].src = `${baseUrl}${imgCincoRecomendaciones[i]}`;
+                    imagenes[i].alt = `${tituloCincoRecomendaciones[i]}`;
+                    a[i].href+=`?id=${idsCincoRecomendaciones[i]}`;
+                };
+                for (let i=0; i<divs.length; i++){
+                    divs[i].style.display= 'inline-block';
+                };    
+            });
+
+
+        })
+        .catch(function(error){
+            console.log('El error es: ' + error)
+        })
+
+    fetch(`https://api.themoviedb.org/3/movie/${idPelicula}/reviews?api_key=${apiKey}&language=es`)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            let resenasPeliculas= data.results;
+
+            let botonRes= document.querySelector('#resenas_boton');
+            let resenasSect= document.querySelector('.resenas');
+            let listaLi= resenasSect.querySelector('div ul li');
+
+            if(resenasPeliculas.length>0){
+                for(let i=0; i<resenasPeliculas.length; i++){
+                    let autor= resenasPeliculas[i].author;
+                    let comentario= resenasPeliculas[i].content;
+
+                    if(i==0){
+                        listaLi.innerHTML=`<b>${autor}</b>: ${comentario}`
+                    }else{
+                        resenasSect.innerHTML+=`<div>
+                                                    <ul>
+                                                        <li><b>${autor}</b>: ${comentario}</li>
+                                                    </ul>
+                                                </div>`
+                    };
+                };
+
+                let divsResenas= resenasSect.querySelectorAll('div');
+
+                for(let i=0; i<divsResenas.length; i++){
+                    botonRes.addEventListener('click', function(){
+                        divsResenas[i].style.display= 'inline-block';
+                    });
+                };
+
+            }else{
+                listaLi.innerHTML=`<strong>Lo siento, no hay reseñas disponibles :(</strong>`
+                let div= resenasSect.querySelector('div');
+                botonRes.addEventListener('click', function(){
+                    div.style.display= 'inline-block';
+                    div.style.backgroundColor= 'Bisque';
+                    listaLi.style.color= 'rgb(247, 134, 134)';
+                });
+            };
+
+        })
+        .catch(function(error){
+            console.log('El error es: ' + error)
+        })
+
+        fetch(`https://api.themoviedb.org/3/movie/${idPelicula}/videos?api_key=${apiKey}&language=es`)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data)
+                let trailer= data.results[0];
+                let trailerId= trailer.id;
+                let youtube= trailer.site;
+
+                let trailerSect= document.querySelector('.trailer');
+
+                if (youtube === 'YouTube') {
+                    let trailerUrl = 'https://www.youtube.com/watch?v=' + trailerId;
+                    let iframe= trailerSect.querySelector('iframe');
+                    iframe.src=`${trailerUrl}`;
+                    iframe.style.display='inline-block'
+                } else {
+                    let parrafo= trailerSect.querySelector('p');
+                    parrafo.innerHTML=`<strong>Lo siento, no hay trailer disponible para esta película :(</strong>`;
+                    parrafo.style.display='inline-block';
+                };
+            })
+            .catch(function(error){
+                console.log('El error es: ' + error)
+            })
+})
