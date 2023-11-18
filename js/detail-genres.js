5af6d20f9b337af1ca9b58800ec7d758178a70e4
jazbang <jbang@udesa.edu.ar>
2023-11-17 23:33:37 -0300

    punto 5 generos completo

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,59 +1,71 @@
-let apiKey = 'd4da6f83d8fa5dad990cafe88cb4fbf7'
-let urlparams = new URLSearchParams(location.search);
-let qsIdGenero = urlparams.get('id');
-let DiscoverEndpointPelis = `https://api.themoviedb.org/3/discover/movie`;
-let DiscoverEndpointSeries = `https://api.themoviedb.org/3/discover/tv`;
+document.addEventListener('DOMContentLoaded', function () {
+	let apiKey = 'd4da6f83d8fa5dad990cafe88cb4fbf7'
+	let queryStringObj = new URLSearchParams(location.search);
+	let genero= queryStringObj.get('name');
+    let nombreGenero=queryStringObj.get('ng');
+	let discoverEndpointPelis = `https://api.themoviedb.org/3/discover/movie`;
+	let discoverEndpointSeries = `https://api.themoviedb.org/3/discover/tv`;
 
-let detailGenresPelis = `${DiscoverEndpointPelis}?api_key=${apiKey}&with_genres=${qsIdGenero}`;
+	let detailGenresPelis = `${discoverEndpointPelis}?api_key=${apiKey}&with_genres=${genero}`;
 
-fetch(detailGenresPelis)
-	.then(function(response){
-		return response.json();
- 	})
-	.then(function(data){
-		console.log(data);
-		let miData = data.results;
-		contenido = ' ';
-		
-		for (let i = 0; i < 5; i++) {
-			contenido +=`<div class="detail-genres">
-							<h3>${miData[i].original_title}</h3>
-							<a href="detail-movie.html?id=${miData[i].id}"><img src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path} " alt="${miData[i].original_title}"></a>
-						</div>`
-	    }
+	fetch(detailGenresPelis)
+		.then(function(response){
+			return response.json();
+ 		})
+		.then(function(data){
+			console.log(data);
+			let miData = data.results;
+			document.querySelector('.seudoBody').innerHTML=nombreGenero
+			contenido = ' ';
+			if (miData[0]){
+				for (let i = 0; i < 5; i++) {
+					contenido +=`<div class="detail-genres">
+									<h3>${miData[i].original_title}</h3>
+									<a href="detail-movie.html?id=${miData[i].id}"><img src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path} " alt="${miData[i].original_title}"></a>
+								</div>`
+	    		};
 
-		let sectGenres= document.querySelector('.detalleGeneros')
-		sectGenres.innerHTML = contenido;
-})
+				let sectGenres= document.querySelector('.detalleGeneros')
+				sectGenres.innerHTML = contenido;
+			}else{
+				sectGenres.innerHTML='No se encontraron resultados para este género :(';
+			};
+	})
 
-.catch(function(error){
- console.log('El error es: ' + error);
-})
+	.catch(function(error){
+ 		console.log('El error es: ' + error);
+	})
 
 
 
-let detailGenresSeries = `${DiscoverEndpointSeries}?api_key=${apiKey}&with_genres=${qsIdGenero}`;
+	let detailGenresSeries = `${discoverEndpointSeries}?api_key=${apiKey}&with_genres=${genero}`;
 
-fetch(detailGenresSeries)
-	.then(function(response){
-	  return response.json();
-    })
-    .then(function(data){
-		console.log(data);
-		let miData = data.results;
-	 	contenido = ' ';
-		for (let i = 0; i < 5; i++) {
-			contenido +=`<div class="detail-genres">
-							<h3>${miData[i].original_title}</h3>
-							<a href="detail-movie.html?id=${miData[i].id}"><img src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path} " alt="${miData[i].original_title}"></a>
-						</div>`
-	    }
+	fetch(detailGenresSeries)
+		.then(function(response){
+	  		return response.json();
+    	})
+    	.then(function(data){
+			console.log(data);
+			let miData = data.results;
+			document.querySelector('.seudoBody').innerHTML=nombreGenero
+			contenido = ' ';
+			if (miData[0]){
+				for (let i = 0; i < 5; i++) {
+					contenido +=`<div class="detail-genres">
+									<h3>${miData[i].original_title}</h3>
+									<a href="detail-serie.html?id=${miData[i].id}"><img src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path} " alt="${miData[i].original_title}"></a>
+								</div>`
+	    		};
 
-		let sectGenres= document.querySelector('.detalleGeneros')
-		sectGenres.innerHTML = contenido;
+				let sectGenres= document.querySelector('.detalleGeneros')
+				sectGenres.innerHTML = contenido;
+			}else{
+				sectGenres.innerHTML='No se encontraron resultados para este género :(';
+			};
 
-})
+	})
 
-.catch(function(error){
-	console.log('El error es: ' + error);
-})
+	.catch(function(error){
+		console.log('El error es: ' + error);
+	})
+});
